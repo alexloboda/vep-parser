f1dec7eb52f708be62cad7177eca72d57d2bf853
Alexander Loboda <aleks.loboda@gmail.com>
2024-05-24 12:59:53 +0300

    Support for non sites-only VCFs

--- vep_parser.py
+++ vep_parser.py
@@ -18,24 +18,36 @@ class VepParser:
                     continue
                 self.parse_line(line.rstrip())
 
+    def extract_vep(self, info):
+        csq = None
+        fields = info.split(";")
+        for field in fields:
+            if field.startswith("CSQ="):
+                csq = field[4:]
+                break
+        if csq is None:
+            print("Could not find CSQ field in the INFO column")
+            exit(1)
+        transcripts = csq.split(",")
+        result = []
+        for t in transcripts:
+            tokens = t.split("|")
+            if len(tokens) != len(self.fields):
+                print("Number of fields in CSQ does not match the number of fields in the argument")
+                print("Info field " + info)
+                exit(1)
+            result.append(dict(zip(self.fields, tokens)))
+        return result
+
     def parse_line(self, line):
         vcf_fields = line.split("\t")
         variant = vcf_fields[0:7]
         variant_dict = dict((self.vcf_fields[i], variant[i]) for i in range(len(variant)))
-        print(vcf_fields)
         info = vcf_fields[7]
         if info == ".":
             return
-        vep = re.split("[|,]", info)
-        if len(vep) % len(self.fields) != 0:
-            print("Could not parse the line: ")
-            print(line)
-            print("VEP entry length - %d while the arg states %d" % (len(vep), len(self.fields)))
-            exit(1)
-        n_entries = len(vep) // len(self.fields)
-        for i in range(n_entries):
-            transcript = vep[i * len(self.fields):(i + 1) * len(self.fields)]
-            annotation = dict(zip(self.fields, transcript))
+        vep = self.extract_vep(info)
+        for annotation in vep:
             gene = annotation['SYMBOL']
             if gene == '':
                 continue
